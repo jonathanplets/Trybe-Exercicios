835df99e85472fe22da7b71920e1ee29a4c26e64
Jonathan <jonathanplets@gmail.com>
2021-07-29 00:28:17 -0300

    terceira questao

---
+++ fundamentos/dia-1-javascript/questao3.js
@@ -0,0 +1,11 @@
+let numero1 = 40
+let numero2 = 7
+let numero3 = 4
+
+if(numero1 > numero2 && numero1 > numero3){
+    console.log("o maior numero é o numero1")
+}else if(numero2 > numero1 && numero2 > numero3){
+    console.log("o maior numero é o numero2")
+}else{
+    console.log("o maior numero é o numero3")
+}
