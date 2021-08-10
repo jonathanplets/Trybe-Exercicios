6d17714aa44d8101c959f44a9611b15dbd3c6bf7
Jonathan <jonathanplets@gmail.com>
2021-08-10 14:51:54 -0300

    atividadeDOM

---
+++ primeirosPassos/exercicioDOM.js
@@ -0,0 +1,4 @@
+console.log(document.getElementById("texto").innerText)
+console.log(document.getElementById("main-content").style.backgroundColor = 'rgb(76,164,109');
+console.log(document.getElementById("center-content").style.backgroundColor = 'rgb(255,255,255)');
+console.log(document.getElementById("titulo").innerText = ("Exercício 5.1 - JavaScript"));
